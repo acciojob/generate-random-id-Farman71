dd8edaf5951d8bc171a00feeca41303bcf4e5e51
Farman71 <farmanhussain71@gmail.com>
2025-01-20 12:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function makeid(l) {
-  // write your code here
-}
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+function makeid(l) {
+    // write your code here
+      let result ="";
+      let char='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let charLength=char.length;
+      for(let i=0;i<l;i++)
+      {
+          let randIndex=Math.floor(Math.random()*charLength);	
+          
+		  result=result+char[randIndex];
+      }
+      return result;
+  }
+  
+  // Do not change the code below.
+  const l = prompt("Enter a number.");
+  alert(makeid(l));
+
